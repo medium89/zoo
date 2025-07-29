a4ef1807a65319abeccb5f177ccfbf71d8c8425a
medium89 <ivanovdo@bk.ru>
2025-07-29 17:14:48 +0700

    Add maintenance mode setting

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
+use Illuminate\Http\Request;
+
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $siteClosed = Setting::where('key', 'site_closed')->value('value');
+        return view('admin.dashboard', [
+            'siteClosed' => (bool) $siteClosed,
+        ]);
+    }
+
+    public function saveSiteStatus(Request $request)
+    {
+        Setting::updateOrCreate(
+            ['key' => 'site_closed'],
+            ['value' => $request->has('site_closed') ? '1' : '0']
+        );
+
+        return redirect()->route('admin.index')->with('success', 'Настройки сохранены');
     }
 }
---
+++ resources/views/closed.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="d-flex justify-content-center align-items-center" style="min-height:100vh;">
+    <h1>Сайт закрыт на ремонт :)</h1>
+</div>
+@endsection
