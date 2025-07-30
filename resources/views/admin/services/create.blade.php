b99d5ad13d312d95e81d7aa10094df1ebb4fc13b
Дмитрий <animatrop@gmail.com>
2025-07-30 19:41:37 +0700

    исправил редактор висивиг на 6 версию

--- resources/views/admin/services/create.blade.php
+++ resources/views/admin/services/create.blade.php
@@ -28,7 +28,7 @@
     <a href="{{ route('admin.services.index') }}" class="btn btn-secondary">Назад</a>
 </form>
 <!-- Подключение TinyMCE WYSIWYG редактора -->
-<script src="https://cdn.tiny.cloud/1/ilf8e4vsikngopxe08xuqeely1o5rigddts9einhhrfen31e/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+<script src="https://cdn.tiny.cloud/1/ilf8e4vsikngopxe08xuqeely1o5rigddts9einhhrfen31e/tinymce/6/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
 <script>
     tinymce.init({
         selector: 'textarea.wysiwyg',
