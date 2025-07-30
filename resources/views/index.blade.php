6f25db3dd9ec77cfa9cc94c3bd28a231348a7679
medium89 <ivanovdo@bk.ru>
2025-07-30 17:33:52 +0700

    Add floating social contact bar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
     @include('sections.header')
+    @include('sections.social-bar')
     @include('sections.slider')
     @include('sections.about')
     @include('sections.advantages')
