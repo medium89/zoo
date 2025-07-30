1e87290fa91bb0fc5455907c3f0c7efab1ffb6cd
medium89 <ivanovdo@bk.ru>
2025-07-30 18:02:37 +0700

    feat: add reopen button for social bar

--- resources/views/sections/social-bar.blade.php
+++ resources/views/sections/social-bar.blade.php
@@ -1,4 +1,7 @@
 @if(isset($socials) && $socials->count() > 0)
+<button class="social-bar-open" id="socialBarOpen" aria-label="open socials">
+    <i class="fas fa-chevron-left"></i>
+</button>
 <div class="social-bar" id="socialBar">
     <button class="social-bar-toggle" id="socialBarToggle" aria-label="toggle socials">
         <i class="fas fa-chevron-right"></i>
