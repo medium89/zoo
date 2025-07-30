17d0d52354e3e6ed383b40a6f717a17852f9e146
medium89 <ivanovdo@bk.ru>
2025-07-30 17:31:49 +0700

    feat(admin): add wysiwyg editor

--- resources/views/admin/socials/edit.blade.php
+++ resources/views/admin/socials/edit.blade.php
@@ -53,7 +53,7 @@
     </div>
     <div class="mb-3">
         <label for="text" class="form-label">Текст</label>
-        <input type="text" class="form-control" id="text" name="text" value="{{ $social->text }}" required>
+        <textarea class="form-control wysiwyg" id="text" name="text" rows="4" required>{{ $social->text }}</textarea>
     </div>
     <div class="mb-3">
         <label for="order" class="form-label">Порядок</label>
@@ -118,16 +118,28 @@
         });
 
         // Поиск иконок
-        iconSearchInput.addEventListener('keyup', (e) => {
-            renderIcons(e.target.value);
-        });
+    iconSearchInput.addEventListener('keyup', (e) => {
+        renderIcons(e.target.value);
+    });
 
-        // Закрытие модального окна при клике вне его (если нужно, можно добавить)
+    // Закрытие модального окна при клике вне его (если нужно, можно добавить)
         // iconPickerModal.addEventListener('click', (e) => {
         //     if (e.target === iconPickerModal) {
-        //         iconPickerModal.style.display = 'none';
-        //     }
-        // });
+    //         iconPickerModal.style.display = 'none';
+    //     }
+    // });
+});
+</script>
+<!-- Подключение TinyMCE WYSIWYG редактора -->
+<script src="https://cdn.tiny.cloud/1/ilf8e4vsikngopxe08xuqeely1o5rigddts9einhhrfen31e/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+<script>
+    tinymce.init({
+        selector: 'textarea.wysiwyg',
+        menubar: false,
+        plugins: 'link lists code',
+        toolbar: 'undo redo | bold italic underline | bullist numlist | link | code',
+        language: 'ru',
+        height: 300
     });
 </script>
-@endsection 
+@endsection
