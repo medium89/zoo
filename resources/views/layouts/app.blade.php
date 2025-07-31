33a941c3a42ae636a56159c22b264fb3322bc387
Дмитрий <animatrop@gmail.com>
2025-07-31 13:04:32 +0700

    установка tinymce через зависимости

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="/build/assets/app-CMF_8PtE.css">
+    <script type="module" src="/build/assets/app-CTskaXw4.js"></script>
 </head>
 <body>
     @yield('content')
