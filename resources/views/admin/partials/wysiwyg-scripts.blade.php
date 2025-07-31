16c61241bb780ad5aa184cc6bfcd4d783ee6c4cf
Дмитрий <animatrop@gmail.com>
2025-07-31 20:45:39 +0700

    настройка

--- resources/views/admin/partials/wysiwyg-scripts.blade.php
+++ resources/views/admin/partials/wysiwyg-scripts.blade.php
@@ -1,3 +1,3 @@
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+<script src="https://cdn.tiny.cloud/1/ilf8e4vsikngopxe08xuqeely1o5rigddts9einhhrfen31e/tinymce/6/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/tinymce@6/plugins/emoticons/js/emojis.min.js" defer></script>
 <script src="{{ asset('js/wysiwyg.js') }}" defer></script>
