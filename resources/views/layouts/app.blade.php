e0d90d0f2f9385d9288481a7b4e87b219581a273
Дмитрий <animatrop@gmail.com>
2025-07-31 13:21:08 +0700

    установка tinymce через зависимости

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,17 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="{{ $settings->description ?? '' }}">
     <meta name="robots" content="{{ $settings->robots ?? '' }}">
+
     <meta property="og:title" content="{{ $settings->og_title ?? 'Заголовок страницы' }}">
     <meta property="og:description" content="{{ $settings->og_description ?? 'Описание' }}">
     <meta property="og:image" content="{{ $settings->og_image ?? 'https://example.com/image.jpg' }}">
     <meta property="og:url" content="{{ $settings->og_url ?? 'https://example.com/page' }}">
+
     <title>Зооняня</title>
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="/build/assets/app-CMF_8PtE.css">
+    <script type="module" src="/build/assets/app-CTskaXw4.js"></script>
 </head>
 <body>
     @yield('content')
