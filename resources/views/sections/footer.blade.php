50ddc1d14b846e6e91f54c7f24c64ab95696a905
Дмитрий <animatrop@gmail.com>
2025-08-04 15:53:28 +0700

    yandex map

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,6 +1,9 @@
 <footer>
 
-    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Af65e8307b40f10df603f7a788a3c051606fd92405aef27868560308c0349b593&amp;width=100%25&amp;height=600&amp;lang=ru_RU&amp;scroll=true"></script>
+<script type="text/javascript" charset="utf-8" async 
+  src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Af65e8307b40f10df603f7a788a3c051606fd92405aef27868560308c0349b593&amp;width=100%25&amp;height=600&amp;lang=ru_RU&amp;scroll=false&amp;drag=false">
+</script>
+
 
     <div class="footer-container container">
         <div class="footer-content">
